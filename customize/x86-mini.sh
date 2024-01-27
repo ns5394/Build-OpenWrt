1b77709895d67cef85698556f5e33cfaaa36c03f
NIES <62430434+ns5394@users.noreply.github.com>
2024-01-27 23:49:17 +0800

    Update x86-mini.sh

--- customize/x86-mini.sh
+++ customize/x86-mini.sh
@@ -8,8 +8,8 @@
 # 添加额外软件包
 git clone https://github.com/kenzok8/openwrt-packages openwrt/kenzo
 git clone https://github.com/kenzok8/small openwrt/small
-mv openwrt/kenzo/* openwrt/package/
-mv openwrt/small/* openwrt/package/
+mv -f openwrt/kenzo/* openwrt/package/
+mv -f openwrt/small/* openwrt/package/
 rm -rf openwrt/kenzo
 rm -rf openwrt/small
 
@@ -20,7 +20,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 sed -i 's|^root:::0:99999:7:::|root:$1$0JEhwicN$y./59TClu6QWLU3lAwJWi/:19733:0:99999:7:::|' openwrt/package/base-files/files/etc/shadow
 
 # 更改Argone主题背景 设为默认
-cp -f $GITHUB_WORKSPACE/customize/images/bg1.jpg openwrt/feeds/kenzo/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/customize/images/bg1.jpg openwrt/package/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
 sed -i 's/bootstrap/argone/g' openwrt/feeds/luci/collections/luci/Makefile
 
 # 更改banner
@@ -45,5 +45,5 @@ sed -i 's/vpn/services/g; s/VPN/Services/g' openwrt/feeds/luci/applications/luci
 sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
 # 调整alist到 服务 菜单
-sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/controller/alist.lua
-sed -i 's/nas/services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/view/alist/alist_status.htm
+sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/package/luci-app-alist/luasrc/controller/alist.lua
+sed -i 's/nas/services/g' openwrt/package/luci-app-alist/luasrc/view/alist/alist_status.htm
