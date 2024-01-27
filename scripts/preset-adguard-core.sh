094f86b16320e2ce8546aa3e1384beb32c57965c
NIES <62430434+ns5394@users.noreply.github.com>
2024-01-28 00:11:59 +0800

    Update preset-adguard-core.sh

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -7,7 +7,3 @@ AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/release
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
 
 chmod +x files/usr/bin/AdGuardHome
-
-cd files/usr/bin
-echo "usr/bin文件夹目录："
-ls
