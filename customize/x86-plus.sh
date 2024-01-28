c8f79c8e2047c8c1d68da85c1fe6405784dfb767
NIES <62430434+ns5394@users.noreply.github.com>
2024-01-28 22:57:19 +0800

    Update x86-plus.sh

--- customize/x86-plus.sh
+++ customize/x86-plus.sh
@@ -11,11 +11,12 @@
 
 # 更改默认IP地址
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-# 更改设备名
-sed -i 's/OpenWrt/N4100/g' openwrt/package/base-files/files/bin/config_generate
 
-# 设置密码为991016
-sed -i 's|^root:::0:99999:7:::|root:$1$0JEhwicN$y./59TClu6QWLU3lAwJWi/:19733:0:99999:7:::|' openwrt/package/base-files/files/etc/shadow
+# 更改设备名
+# sed -i 's/OpenWrt/N4100/g' openwrt/package/base-files/files/bin/config_generate
+
+# 清除默认登录密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # 更改Argone主题背景
 cp -f $GITHUB_WORKSPACE/customize/mages/bg1.jpg openwrt/feeds/kenzo/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
@@ -35,9 +36,9 @@ sed -i 's|/bin/login|/bin/login -f root|g' openwrt/feeds/packages/utils/ttyd/fil
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' openwrt/package/lean/autocore/files/*/index.htm
 
 # 调整 ZeroTier 到 服务 菜单
-#sed -i 's/vpn/services/g; s/VPN/Services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-#sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
+# sed -i 's/vpn/services/g; s/VPN/Services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+# sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
 # 调整alist到 服务 菜单
-#sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/controller/alist.lua
-#sed -i 's/nas/services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/view/alist/alist_status.htm
+# sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/controller/alist.lua
+# sed -i 's/nas/services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/view/alist/alist_status.htm
