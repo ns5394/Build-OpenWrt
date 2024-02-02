45348543db1526655d8f4f2b187dffb15d8a100f
NIES <62430434+ns5394@users.noreply.github.com>
2024-02-02 22:59:31 +0800

    Update preset-psswall.sh

--- scripts/preset-psswall.sh
+++ scripts/preset-psswall.sh
@@ -10,6 +10,7 @@ wget -qO- $Brook_URL > files/usr/bin/brook
 wget -qO- $Hysteria_URL > files/usr/bin/hysteria
 
 LATEST_SingBox=$(curl -sI "https://github.com/SagerNet/sing-box/releases/latest" | grep -i "location:" | awk -F "/" '{gsub(/^v/, "", $NF); print $NF}' | tr -d '\r')
+
 # 根据不同的架构执行不同的命令
 if [ "${ARCH}" == "amd64" ]; then
   ChinaDNS_URL="https://github.com/zfl9/chinadns-ng/releases/latest/download/chinadns-ng-x86_64"
@@ -37,14 +38,6 @@ wget -O "sing-box-${LATEST_SingBox}-linux-${ARCH}.tar.gz" "$SINGBOX_URL"
 tar xzf sing-box-${LATEST_SingBox}-linux-${ARCH}.tar.gz
 mv sing-box-${LATEST_SingBox}-linux-${ARCH}/sing-box files/usr/bin/
 rm -rf sing-box-${LATEST_SingBox}-linux-${ARCH} && rm -rf sing-box-${LATEST_SingBox}-linux-${ARCH}.tar.gz
-
-# 下载最新版本的 v2ray-plugin-------------------------
-wget $(curl -s https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest | grep browser_download_url | grep linux-${ARCH} | cut -d '"' -f 4) -O v2ray-plugin.tar.gz
-tar -zxvf v2ray-plugin.tar.gz
-mv v2ray-plugin_linux_${ARCH} v2ray-plugin
-mv v2ray-plugin files/usr/bin/
-rm -rf v2ray-plugin.tar.gz
----------------------------------------------------
 
 
 chmod +x files/usr/bin/*
