5b9df9fec030b249862b6007d394163fed4f63d9
NIES <62430434+ns5394@users.noreply.github.com>
2024-08-19 20:29:17 +0800

    Update x86-plus.sh

--- customize/x86-plus.sh
+++ customize/x86-plus.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #=================================================
 # Description: DIY script
@@ -6,11 +7,15 @@
 #=================================================
 
 # 添加额外软件包
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> openwrt/feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >> openwrt/feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages openwrt/kenzo
+git clone https://github.com/kenzok8/small openwrt/small
+mv -f openwrt/kenzo/* openwrt/package/
+mv -f openwrt/small/* openwrt/package/
+rm -rf openwrt/kenzo
+rm -rf openwrt/small
 
 # 更改默认IP地址
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 更改设备名
 # sed -i 's/OpenWrt/N4100/g' openwrt/package/base-files/files/bin/config_generate
@@ -18,8 +23,12 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 # 清除默认登录密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-# 更改Argone主题背景
-cp -f $GITHUB_WORKSPACE/customize/mages/bg1.jpg openwrt/feeds/kenzo/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
+# 更改Argone主题背景 设为默认
+cp -f $GITHUB_WORKSPACE/customize/images/bg1.jpg openwrt/package/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
+sed -i 's/bootstrap/argone/g' openwrt/feeds/luci/collections/luci/Makefile
+
+# 更改banner
+cp -f $GITHUB_WORKSPACE/customize/diy/banner openwrt/package/base-files/files/etc/banner
 
 # x86 型号只显示 CPU 型号
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' openwrt/package/lean/autocore/files/x86/autocore
@@ -36,9 +45,9 @@ sed -i 's|/bin/login|/bin/login -f root|g' openwrt/feeds/packages/utils/ttyd/fil
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' openwrt/package/lean/autocore/files/*/index.htm
 
 # 调整 ZeroTier 到 服务 菜单
-# sed -i 's/vpn/services/g; s/VPN/Services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-# sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
+sed -i 's/vpn/services/g; s/VPN/Services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
 # 调整alist到 服务 菜单
-# sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/controller/alist.lua
-# sed -i 's/nas/services/g' openwrt/feeds/kenzo/luci-app-alist/luasrc/view/alist/alist_status.htm
+# sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/package/luci-app-alist/luasrc/controller/alist.lua
+# sed -i 's/nas/services/g' openwrt/package/luci-app-alist/luasrc/view/alist/alist_status.htm
