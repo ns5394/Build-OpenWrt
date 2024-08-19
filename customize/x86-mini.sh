3f4cf66c2fa6166b33801299050acda7651b9325
NIES <62430434+ns5394@users.noreply.github.com>
2024-08-19 21:13:08 +0800

    Update x86-mini.sh

--- customize/x86-mini.sh
+++ customize/x86-mini.sh
@@ -14,7 +14,7 @@ rm -rf openwrt/kenzo
 rm -rf openwrt/small
 
 # 更改默认IP地址
-sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 更改设备名
 # sed -i 's/OpenWrt/N4100/g' openwrt/package/base-files/files/bin/config_generate
