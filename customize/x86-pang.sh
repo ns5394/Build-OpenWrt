9be2a75d8215e2acac9be0ec8ea8ad124252ac8b
NIES <62430434+ns5394@users.noreply.github.com>
2025-01-05 10:31:53 +0800

    Update x86-pang.sh

--- customize/x86-pang.sh
+++ customize/x86-pang.sh
@@ -14,7 +14,7 @@ rm -rf openwrt/kenzo
 rm -rf openwrt/small
 
 # 更改默认IP地址
-sed -i 's/192.168.1.1/192.168.5.2/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.4/g' openwrt/package/base-files/luci2/bin/config_generate
 
 # 更改设备名
 # sed -i 's/OpenWrt/N4100/g' openwrt/package/base-files/files/bin/config_generate
@@ -23,8 +23,9 @@ sed -i 's/192.168.1.1/192.168.5.2/g' openwrt/package/base-files/files/bin/config
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # 更改Argone主题背景 设为默认
-cp -f $GITHUB_WORKSPACE/customize/images/bg1.jpg openwrt/package/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
-sed -i 's/bootstrap/argone/g' openwrt/feeds/luci/collections/luci/Makefile
+cp -f $GITHUB_WORKSPACE/customize/images/bg1.jpg openwrt/package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+sed -i 's/bootstrap/argon/g' openwrtfeeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/g' openwrtfeeds/luci/collections/luci-light/Makefile
 
 # 更改banner
 cp -f $GITHUB_WORKSPACE/customize/diy/banner openwrt/package/base-files/files/etc/banner
@@ -44,9 +45,9 @@ sed -i 's|/bin/login|/bin/login -f root|g' openwrt/feeds/packages/utils/ttyd/fil
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' openwrt/package/lean/autocore/files/*/index.htm
 
 # 调整 ZeroTier 到 服务 菜单
-sed -i 's/vpn/services/g; s/VPN/Services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
+#sed -i 's/vpn/services/g; s/VPN/Services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+#sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
 # 调整alist到 服务 菜单
-sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/package/luci-app-alist/luasrc/controller/alist.lua
-sed -i 's/nas/services/g' openwrt/package/luci-app-alist/luasrc/view/alist/alist_status.htm
+# sed -i 's/nas/services/g; s/NAS/Services/g' openwrt/package/luci-app-alist/luasrc/controller/alist.lua
+# sed -i 's/nas/services/g' openwrt/package/luci-app-alist/luasrc/view/alist/alist_status.htm
