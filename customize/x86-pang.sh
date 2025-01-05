0f8ad7f0db24e44d8725da5bc78a46ac874d09be
NIES <62430434+ns5394@users.noreply.github.com>
2025-01-05 10:43:28 +0800

    Update x86-pang.sh

--- customize/x86-pang.sh
+++ customize/x86-pang.sh
@@ -14,7 +14,7 @@ rm -rf openwrt/kenzo
 rm -rf openwrt/small
 
 # 更改默认IP地址
-sed -i 's/192.168.1.1/192.168.5.4/g' openwrt/package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.3/g' openwrt/package/base-files/luci2/bin/config_generate
 
 # 更改设备名
 # sed -i 's/OpenWrt/N4100/g' openwrt/package/base-files/files/bin/config_generate
