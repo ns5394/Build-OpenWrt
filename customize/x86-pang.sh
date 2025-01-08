b4b26baa9b33c0a590d1a1041cdb80177ca1fac8
NIES <62430434+ns5394@users.noreply.github.com>
2025-01-08 10:17:49 +0800

    Update x86-pang.sh

--- customize/x86-pang.sh
+++ customize/x86-pang.sh
@@ -14,8 +14,8 @@ rm -rf openwrt/kenzo
 rm -rf openwrt/small
 
 # 更改默认IP地址
-sed -i 's/192.168.1.1/192.168.5.3/g' openwrt/package/base-files/file/bin/config_generate
-sed -i 's/192.168.1.1/192.168.5.3/g' openwrt/package/base-files/luci2/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.3/g' openwrt/package/base-files/file/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.3/g' openwrt/package/base-files/luci2/bin/config_generate
 sed -i \
     -e 's/192\.168\.1\.1/192.168.5.3/' \
     -e '/set network\.\$1\.netmask/a\    set network.$1.gateway='\''192.168.5.1'\''' \
