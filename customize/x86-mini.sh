5c6da43b2b61b1d77aae935b141106d6f106ce8d
NIES <62430434+ns5394@users.noreply.github.com>
2025-01-08 12:29:39 +0800

    Update x86-mini.sh

--- customize/x86-mini.sh
+++ customize/x86-mini.sh
@@ -15,11 +15,11 @@ rm -rf openwrt/small
 
 # 更改默认IP地址
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/file/bin/config_generate
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/luci2/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/luci2/bin/config_generate
 
 # 更改设备名
 sed -i 's/LEDE/OpenWrt/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/LEDE/OpenWrt/g' openwrt/package/base-files/luci2/bin/config_generate
+# sed -i 's/LEDE/OpenWrt/g' openwrt/package/base-files/luci2/bin/config_generate
 
 # 清除默认登录密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
@@ -27,7 +27,7 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 # 更改Argone主题背景 设为默认
 cp -f $GITHUB_WORKSPACE/customize/images/bg1.jpg openwrt/package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 sed -i 's/bootstrap/argone/g' openwrt/feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/argone/g' openwrt/feeds/luci/collections/luci-light/Makefile
+# sed -i 's/bootstrap/argone/g' openwrt/feeds/luci/collections/luci-light/Makefile
 
 # 更改banner
 cp -f $GITHUB_WORKSPACE/customize/diy/banner openwrt/package/base-files/files/etc/banner
