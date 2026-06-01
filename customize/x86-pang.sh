2daa76b77c65ae369a8a8617255f1a26837ac620
NIES <62430434+ns5394@users.noreply.github.com>
2026-06-01 23:13:02 +0800

    Update x86-pang.sh

--- customize/x86-pang.sh
+++ customize/x86-pang.sh
@@ -6,13 +6,13 @@
 #=================================================
 
 # 添加额外软件包
-git clone https://github.com/kenzok8/openwrt-packages openwrt/kenzo
-git clone https://github.com/kenzok8/small openwrt/small
-rm -rf openwrt/feeds/luci/themes/luci-theme-argon
-mv -f openwrt/kenzo/* openwrt/package/
-mv -f openwrt/small/* openwrt/package/
-rm -rf openwrt/kenzo
-rm -rf openwrt/small
+#git clone https://github.com/kenzok8/openwrt-packages openwrt/kenzo
+#git clone https://github.com/kenzok8/small openwrt/small
+#rm -rf openwrt/feeds/luci/themes/luci-theme-argon
+##mv -f openwrt/kenzo/* openwrt/package/
+#mv -f openwrt/small/* openwrt/package/
+#rm -rf openwrt/kenzo
+#rm -rf openwrt/small
 
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git 
 # mv -f luci-app-argon-config openwrt/package/
@@ -46,18 +46,18 @@ cp -f $GITHUB_WORKSPACE/customize/images/bg1.jpg openwrt/feeds/luci/themes/luci-
 cp -f $GITHUB_WORKSPACE/customize/diy/banner openwrt/package/base-files/files/etc/banner
 
 # x86 型号只显示 CPU 型号
-sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' openwrt/package/lean/autocore/files/x86/autocore
+# sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' openwrt/package/lean/autocore/files/x86/autocore
 
 # 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "openwrt/package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by NIES/g" openwrt/package/lean/default-settings/files/zzz-default-settings
+#date_version=$(date +"%y.%m.%d")
+#orig_version=$(cat "openwrt/package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+#sed -i "s/${orig_version}/R${date_version} by NIES/g" openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # TTYD 自动登录
 sed -i 's|/bin/login|/bin/login -f root|g' openwrt/feeds/packages/utils/ttyd/files/ttyd.config
 
 # 修改本地时间格式
-sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' openwrt/package/lean/autocore/files/*/index.htm
+# sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' openwrt/package/lean/autocore/files/*/index.htm
 
 # 调整 ZeroTier 到 服务 菜单
 # sed -i 's/vpn/services/g' openwrt/feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
